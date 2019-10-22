84ac572aa970c0c97dcde6a287fc383641906584
sunjiatong <sunjiatong@thunisoft.com>
2019-10-22 09:42:11 +0800

    java沉淀.js,正则表达式.js

---
+++ 笔记/java沉淀.js
@@ -0,0 +1,19 @@
+/**/
+1.java判断包含关系
+	/*
+		方法:	a.contains(b)
+		实例:
+			public class pratise {
+
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+					String a = "l love feng ye";
+					String b = "love";
+					boolean c = a.contains(b);
+					System.out.println(a.contains(b));
+					//assertEquals(a.contains(b), "true");
+					assert(c)=true;//加断言
+				}
+			}
+	*/
+	
